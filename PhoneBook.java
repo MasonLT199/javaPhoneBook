c62a7ce92d3807c1ff32f38f1736612ac5105c20
Mason Timmerman <masonlt199@gmail.com>
2017-12-13 14:29:37 -0600

    reupload

--- PhoneBook.java
+++ PhoneBook.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class PhoneBook {
     public static void main(String[] args)
 
-        
+
 
         //standard input object
         Scanner sc = new Scanner(System.in);
@@ -19,23 +19,23 @@ public class PhoneBook {
         //ask for each value and scan
         System.out.println("\nPlease enter your information for the Java Phone Book");
         //first name
-        System.out.print(" First Name: ");
+        System.out.print("  First Name: ");
         String firstName = sc.nextLine();
 
         //middle initial
-        System.out.print(" Middle Initial: ");
+        System.out.print("  Middle Initial: ");
         String middleInitial = sc.nextLine();
 
         //last name
-        System.out.print(" Last Name: ");
+        System.out.print("  Last Name: ");
         String lastName = sc.nextLine();
 
         //age
-        System.out.print(" Age: ");
+        System.out.print("  Age: ");
         int age = sc.nextInt();
 
         //phone number
-        System.out.print(" Phone Number: ");
+        System.out.print("  Phone Number: ");
         long phoneNumber = sc.nextLong();
 
         //send the values read to the NewEntry class and create an object
