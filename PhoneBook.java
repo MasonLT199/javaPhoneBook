e36aabf4c2879843fd36860e02eee2d9d60582cd
Mason Timmerman <masonlt199@gmail.com>
2017-12-13 15:21:06 -0600

    added option menu

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,45 +1,59 @@
-/*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*
-|                                                           |
-|                     Java Phone Book                       |
-|         Created by: Mason Timmerman and Max DeVos         |
-|                     December 2017                         |
-|                                                           |
-*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*
+ |                                                           |
+ |                     Java Phone Book                       |
+ |         Created by: Mason Timmerman and Max DeVos         |
+ |                     December 2017                         |
+ |                                                           |
+ *|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 
 import java.util.Scanner;
 
 public class PhoneBook {
     public static void main(String[] args){
 
-
-
         //standard input object
         Scanner sc = new Scanner(System.in);
 
-        //ask for each value and scan
-        System.out.println("\nPlease enter your information for the Java Phone Book");
-        //first name
-        System.out.print("  First Name: ");
-        String firstName = sc.nextLine();
+        //welcome and option menu
+        System.out.println("\nWelcome to the Java Phone Book by Mason and Max!");
+        System.out.println("\n  Enter what you would like to do with the corresponding number: ");
+        System.out.print("   1 - Search \n   2 - Make a new entry \n   3 - Quit");
+        int option = sc.nextInt();
 
-        //middle initial
-        System.out.print("  Middle Initial: ");
-        String middleInitial = sc.nextLine();
+        if(option == 1) {
+            System.out.println("Sorry, this feature is currrently under construction.");
+        }
 
-        //last name
-        System.out.print("  Last Name: ");
-        String lastName = sc.nextLine();
+        else if(option == 2) {
+            //ask for each value and scan
+            System.out.println("\nPlease enter the information for the new entry");
+            //first name
+            System.out.print("  First Name: ");
+            String firstName = sc.nextLine();
 
-        //age
-        System.out.print("  Age: ");
-        int age = sc.nextInt();
+            //middle initial
+            System.out.print("  Middle Initial: ");
+            String middleInitial = sc.nextLine();
 
-        //phone number
-        System.out.print("  Phone Number: ");
-        long phoneNumber = sc.nextLong();
+            //last name
+            System.out.print("  Last Name: ");
+            String lastName = sc.nextLine();
 
-        //send the values read to the NewEntry class and create an object
-        NewEntry person = new NewEntry(firstName, middleInitial, lastName, age, phoneNumber);
+            //age
+            System.out.print("  Age: ");
+            int age = sc.nextInt();
 
+            //phone number
+            System.out.print("  Phone Number: ");
+            long phoneNumber = sc.nextLong();
+
+            //send the values read to the NewEntry class and create an object
+            NewEntry person = new NewEntry(firstName, middleInitial, lastName, age, phoneNumber);
+
+        }
+
+        else if(option == 3) {
+            return;
+        }
     }
 }
